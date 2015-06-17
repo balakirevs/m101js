98ae478d52099d04d4bca5dc939683dc8f79fd20
juantxokupul <juantxo_kupul@hotmail.com>
2015-06-17 13:09:20 +0200

    Update hw4-2.js

--- Week_4/hw4-2.js
+++ Week_4/hw4-2.js
@@ -1,7 +1,5 @@
 answers:
 
-1. This query performs a collection scan.
+1. The query examines 251120 documents.
 
 2. The query uses an index to determine the order in which to return result documents.
-
-3. The query visits 46462 documents.
